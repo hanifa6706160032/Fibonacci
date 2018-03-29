6fd0efbb476c11ef64a669643cf6ec63de49c608
Hanifa Khoirunnisaa <37887406+hanifa6706160032@users.noreply.github.com>
2018-03-29 09:24:55 +0700

    Update DeretFibonacci.java

--- DeretFibonacci/src/deretfibonacci/DeretFibonacci.java
+++ DeretFibonacci/src/deretfibonacci/DeretFibonacci.java
@@ -20,20 +20,27 @@ public class DeretFibonacci {
         
         // TODO code application logic here
         Scanner show = new Scanner(System.in);
+        
+        int n;
+        int fib[];
+        int jumlah = 0;
+        
+        
         System.out.print("Input :  ");
-        int n = show.nextInt();
-        long fib[] = new long[n];
+        n = show.nextInt();
+        fib = new int[n];
          
         fib[0] = 0;
         fib[1] = 1;
          System.out.println("output : ");
         for(int i = 2; i < n; i++) {
             fib[i] = fib[i-1] + fib[i-2];
-        }
-         
+        }     
         for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
+            jumlah = jumlah + fib[i];
         }
+ 
+         System.out.println(jumlah);
     }
     
 }
